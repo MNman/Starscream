9da80eb16b75e77cb89ea2161e4d6f7674fd1bb7
NanGua <xumengnan@langya.onaliyun.com>
2024-09-27 14:44:01 +0800

    [配置]目录

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
         ],
         dependencies: [],
         targets: [
-            .target(name: "Starscream",resources: [.process("PrivacyInfo.xcprivacy")])
+            .target(name: "Starscream",resources: [.copy("PrivacyInfo.xcprivacy")])
         ]
 )
 
